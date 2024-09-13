3bdd1c406aba2118fcc20005c87b0e430f6a08c2
xJesusx0 <jesuspl0411@gmail.com>
2024-09-12 19:31:05 -0500

    tabla para listar los productos funcionando correctamente

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,80 +1,124 @@
 <template>
 
-    <div class="container mt-4">
-        <h2 class="text-center mb-4">Lista de Productos</h2>
-        <div class="text-center mb-4">
-            <router-link to="/producto" class="btn btn-primary">
-                Agregar Producto
-            </router-link>
-        </div>
-        <div class="table-responsive">
-            <table class="table table-striped table-hover">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Id</th>
-                        <th>Nombre</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody id="table-body">
-                    <!-- datos -->
-                </tbody>
-            </table>
-        </div>
+	<div class="container mt-4">
+		<h2 class="text-center mb-4">Lista de Productos</h2>
 
-    </div>
+		<div class="table-responsive">
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th v-for="header in tableHeaders" :key="header" class="bg-dark text-white text-center">
+							{{ header }}
+						</th>
+					</tr>
+				</thead>
+				<tbody id="table-body">
+					<!-- datos -->
+				</tbody>
+			</table>
+		</div>
+
+	</div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        this.obtenerProductos()
-    },
-    methods: {
-        async obtenerProductos() {
-            try {
-                const response = await fetch('http://localhost:8080/productos')
-                if (!response.ok) {
-                    throw new Error(response.statusText)
-                }
-                const data = await response.json()
+	mounted() {
+		this.obtenerProductos()
+	},
+	data() {
+		return {
+			tableHeaders : [
+				'Id','Nombre','Precio','Cantidad','Acciones'
+			]
+		}
+	},	
+	methods: {
+		async obtenerProductos() {
+			try {
+				const response = await fetch('http://localhost:8080/productos')
+				if (!response.ok) {
+					throw new Error(response.statusText)
+				}
+				const data = await response.json()
 
-                const tableBody = document.getElementById('table-body')
-                tableBody.innerHTML = ''
+				const tableBody = document.getElementById('table-body')
+				tableBody.innerHTML = ''
 
-                data.forEach(producto => {
-                    const tr = document.createElement('tr')
+				data.forEach(producto => {
+					const tr = document.createElement('tr')
 
-                    const id = document.createElement('td')
-                    id.innerText = producto.id
-                    tr.appendChild(id)
+					const id = document.createElement('td')
+					id.innerText = producto.id
+					tr.appendChild(id)
 
-                    const nombre = document.createElement('td')
-                    nombre.innerText = producto.nombre
-                    tr.appendChild(nombre)
+					const nombre = document.createElement('td')
+					nombre.innerText = producto.nombre
+					nombre.classList.add('text-center')
+					tr.appendChild(nombre)
 
-                    const precio = document.createElement('td')
-                    precio.innerText = producto.precio
-                    tr.appendChild(precio)
+					const precio = document.createElement('td')
+					precio.innerText = producto.precio
+					precio.classList.add('text-center')
+					tr.appendChild(precio)
 
-                    const cantidad = document.createElement('td')
-                    cantidad.innerText = producto.cantidad
-                    tr.appendChild(cantidad)
+					const cantidad = document.createElement('td')
+					cantidad.innerText = producto.cantidad
+					cantidad.classList.add('text-center')
+					tr.appendChild(cantidad)
 
-                    const acciones = document.createElement('td')
-                    acciones.innerText = 'Acciones'
-                    tr.appendChild(acciones)
+					const acciones = document.createElement('td')
+					acciones.classList.add('text-center','d-flex','justify-content-around')
 
-                    tableBody.appendChild(tr)
-                })
+					const botonEditar = document.createElement('button')
+					botonEditar.innerText = 'Editar producto'
+					botonEditar.classList.add('btn','btn-warning')
+					botonEditar.addEventListener('click', () => this.redirigir())
+					acciones.appendChild(botonEditar)
 
-            } catch (error) {
-                console.error(error)
-            }
-        }
-    }
+					const botonEliminar = document.createElement('button')
+					botonEliminar.innerText = 'Eliminar producto'
+					botonEliminar.classList.add('btn','btn-danger')
+					botonEliminar.addEventListener('click',() => this.eliminar(producto.id))
+					acciones.appendChild(botonEliminar)
+
+					tr.appendChild(acciones)
+
+					tableBody.appendChild(tr)
+				})
+
+			} catch (error) {
+				console.error(error)
+			}
+		},
+		async eliminar(id) {
+			console.log(id)
+
+			try{
+				const response = await fetch(`http://localhost:8080/productos/${id}`,{
+					method:'DELETE'
+				})
+
+				if(!response.ok){
+					throw new Error(response.statusText)
+				}
+
+				const data = await response.json()
+
+				alert(data.message)
+
+				this.obtenerProductos()
+
+			}catch(error){
+				console.error(error)
+			}
+
+		},
+		redirigir () {
+			this.$router.push('/editar-producto')
+		}
+
+	}
 
 }
 
