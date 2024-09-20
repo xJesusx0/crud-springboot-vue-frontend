4983520aa65dabb2335962ad8ca9747723b8e155
xJesusx0 <jesuspl0411@gmail.com>
2024-09-20 09:04:05 -0500

    se agrego componente de login

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
-import { createApp } from 'vue'
+import { compile, createApp } from 'vue'
 import { createRouter } from 'vue-router'
 import { createWebHashHistory } from 'vue-router'
 
@@ -11,12 +11,14 @@ import App from './App.vue'
 import TableComponent from './components/TableComponent.vue'
 import AddProduct from './components/AddProduct.vue'
 import EditProduct from './components/EditProduct.vue'
+import LoginComponent from './components/LoginComponent.vue'
 
 // rutas
 const routes = [
-    {path:'/', component : TableComponent},
-    {path:'/editar-producto', component : EditProduct},
-    {path:'/agregar-producto', component : AddProduct}
+    { path: '/', component: TableComponent },
+    { path: '/editar-producto', component: EditProduct },
+    { path: '/agregar-producto', component: AddProduct },
+    { path: '/login', component: LoginComponent }
 ]
 
 const urlApi = 'http://localhost:8080'
@@ -27,6 +29,6 @@ const router = createRouter({
 })
 
 const app = createApp(App)
-app.provide('urlApi',urlApi)
+
 app.use(router)
 app.mount('#app')
